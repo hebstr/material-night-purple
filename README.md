cb573fd376649ca3b6c162b8cd1885019a58e03d
hebstr <ju.diez@protonmail.com>
2026-03-16 16:42:20 +0100

    .

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Material Night Purple
 
-A dark purple theme for VSCode and Positron, inspired by [Material Night Eighties](https://open-vsx.org/extension/ms-vscode/Theme-MaterialKit).
+A dark purple theme for VSCode and Positron, inspired by [Material Night Eighties](https://open-vsx.org/extension/ms-vscode/Theme-MaterialKit). Includes Positron-specific styling (console, variables pane, data explorer).
 
 ![Material Night Purple screenshot](screenshots/preview.png)
 
@@ -17,7 +17,7 @@ code --install-extension hebstr.material-night-purple
 ### From source
 
 ```bash
-git clone https://codeberg.org/hebstr/material-night-purple.git
+git clone https://github.com/hebstr/material-night-purple.git
 cd material-night-purple
 npx @vscode/vsce package
 code --install-extension material-night-purple-*.vsix
@@ -25,16 +25,26 @@ code --install-extension material-night-purple-*.vsix
 
 ## Palette
 
-| Role             | Color                                                        |
-| ---------------- | ------------------------------------------------------------ |
-| Background       | ![#1e0e26](https://placehold.co/12x12/1e0e26/1e0e26) `#1e0e26` |
-| Deep background  | ![#14091b](https://placehold.co/12x12/14091b/14091b) `#14091b` |
-| Purple accent    | ![#7d2fa3](https://placehold.co/12x12/7d2fa3/7d2fa3) `#7d2fa3` |
-| Keywords         | ![#c792ea](https://placehold.co/12x12/c792ea/c792ea) `#c792ea` |
-| Strings          | ![#c3e88d](https://placehold.co/12x12/c3e88d/c3e88d) `#c3e88d` |
-| Functions        | ![#82aaff](https://placehold.co/12x12/82aaff/82aaff) `#82aaff` |
-| Numbers          | ![#F78C6C](https://placehold.co/12x12/F78C6C/F78C6C) `#F78C6C` |
-| Comments         | ![#697098](https://placehold.co/12x12/697098/697098) `#697098` |
+### UI colors
+
+| Role             | Color                                                                    |
+| ---------------- | ------------------------------------------------------------------------ |
+| Deep background  | ![#14091b](https://placehold.co/12x12/14091b/14091b) `#14091b` (bg_1)   |
+| Background       | ![#1e0e26](https://placehold.co/12x12/1e0e26/1e0e26) `#1e0e26` (bg_2)   |
+| Surface          | ![#341646](https://placehold.co/12x12/341646/341646) `#341646` (bg_3)   |
+| Subtle purple    | ![#4f2268](https://placehold.co/12x12/4f2268/4f2268) `#4f2268`          |
+| Purple accent    | ![#7d2fa3](https://placehold.co/12x12/7d2fa3/7d2fa3) `#7d2fa3`          |
+| Purple bright    | ![#a73bde](https://placehold.co/12x12/a73bde/a73bde) `#a73bde`          |
+| Purple light     | ![#d08aff](https://placehold.co/12x12/d08aff/d08aff) `#d08aff`          |
+| Cursor           | ![#ffdd00](https://placehold.co/12x12/ffdd00/ffdd00) `#ffdd00`          |
+
+### Syntax colors
+
+| Role     | Color                                                                    |
+| -------- | ------------------------------------------------------------------------ |
+| Comments | ![#a73bde](https://placehold.co/12x12/a73bde/a73bde) `#a73bde`          |
+
+> **Note**: Only comments have custom syntax highlighting. All other token colors inherit from the editor's built-in defaults or user customization.
 
 ## Customization
 
--- build.js
+++ build.js
@@ -1,44 +1,18 @@
 const fs = require("fs");
 
 const p = {
-  bg_deep:       "#14091b",
-  bg:            "#1e0e26",
-  bg_light:      "#341646",
-  purple_muted:  "#4f2268",
-  purple:        "#7d2fa3",
-  purple_bright: "#a73bde",
-  purple_light:  "#d08aff",
-  lavender:      "#c792ea",
-
-  white:         "#ffffff",
-  gray_light:    "#ddd",
-  gray:          "#ccc",
-  text:          "#CDD3DE",
-  near_black:    "#020e14",
-
-  cursor:        "#FFCC00",
-
-  red_light:     "#ff7869",
-  salmon:        "#F77669",
-  red_pink:      "#ff5370",
-  red_dark:      "#d3423e",
-  red_coral:     "#EC5F67",
-
-  orange:        "#ffbc6a",
-  gold:          "#ffcb6b",
-  yellow_bright: "#FAD430",
-  yellow_green:  "#f1e655",
-  yellow_pale:   "#FFEB95",
-  yellow_git:    "#FFCF1B",
-
-  green:         "#c5ff93",
-  green_soft:    "#C3E88D",
-  green_pale:    "#d9f5dd",
-
-  cyan:          "#a2fff6",
-  teal:          "#80CBC4",
-  blue:          "#90e1ff",
-  blue_muted:    "#78ccf0",
+  bg_1:       "#14091b",
+  bg_2:       "#1e0e26",
+  bg_3:       "#341646",
+  purple_1:   "#4f2268",
+  purple_2:   "#7d2fa3",
+  purple_3:   "#a73bde",
+  purple_4:   "#d08aff",
+  fg_1:       "#cccccc",
+  fg_2:       "#cdd3de",
+  fg_3:       "#dddddd",
+  white:      "#ffffff",
+  cursor:     "#ffdd00",
 };
 
 const theme = {
@@ -48,403 +22,160 @@ const theme = {
 
   "colors": {
 
-    "foreground": p.gray_light,
+    "foreground": p.fg_3,
 
-    "tab.inactiveBackground": p.bg_deep,
-    "tab.activeBackground": p.bg_light,
-    "tab.activeBorder": p.purple,
-    "tab.border": p.bg_deep,
+    "tab.inactiveBackground": p.bg_1,
+    "tab.activeBackground": p.bg_3,
+    "tab.activeBorder": p.purple_2,
+    "tab.border": p.bg_1,
     "tab.activeForeground": p.white,
     "tab.inactiveForeground": p.white + "bb",
     "tab.unfocusedActiveForeground": p.white + "cc",
     "tab.unfocusedInactiveForeground": p.white + "99",
 
-    "editor.background": p.bg,
-    "editor.foreground": p.text,
-    "editor.selectionBackground": p.purple,
-    "editor.wordHighlightTextBackground": p.purple + "55",
-    "editor.lineHighlightBackground": p.bg_light,
-    "editorLineNumber.foreground": p.purple_muted,
+    "editor.background": p.bg_2,
+    "editor.foreground": p.fg_2,
+    "editor.selectionBackground": p.purple_2,
+    "editor.wordHighlightTextBackground": p.purple_2 +"55",
+    "editor.lineHighlightBackground": p.bg_3,
+    "editorLineNumber.foreground": p.purple_1,
     "editorLineNumber.activeForeground": p.white,
-    "editorIndentGuide.background1": p.purple_muted,
-    "editorIndentGuide.activeBackground1": p.purple,
+    "editorIndentGuide.background1": p.purple_1,
+    "editorIndentGuide.activeBackground1": p.purple_2,
     "editorCursor.foreground": p.cursor,
-    "editorWidget.background": p.bg,
-    "editorGroupHeader.tabsBackground": p.bg_deep,
-    "editorGroup.border": p.bg_light,
-    "editorOverviewRuler.border": p.purple_muted + "80",
-    "editorInfo.foreground": p.purple_light,
-    "editorLink.activeForeground": p.purple_light,
+    "editorWidget.background": p.bg_2,
+    "editorGroupHeader.tabsBackground": p.bg_1,
+    "editorGroup.border": p.bg_3,
+    "editorOverviewRuler.border": p.purple_1 + "80",
+    "editorInfo.foreground": p.purple_4,
+    "editorLink.activeForeground": p.purple_4,
 
-    "terminal.background": p.bg_deep,
-    "panel.background": p.bg_deep,
-    "panel.border": p.bg_light + "80",
-    "panelTitle.activeBorder": p.purple,
+    "terminal.background": p.bg_1,
+    "panel.background": p.bg_1,
+    "panel.border": p.bg_3 + "80",
+    "panelTitle.activeBorder": p.purple_2,
     "panelTitle.activeForeground": p.white,
     "panelTitle.inactiveForeground": p.white + "b3",
 
-    "positronConsole.background": p.bg_deep,
-    "positronVariables.foreground": p.gray,
-    "positronVariables.background": p.bg_deep,
-    "positronVariables.activeSelectionBackground": p.purple,
-    "positronActionBar.selectBoxBackground": p.bg,
-    "positronActionBar.selectBorder": p.bg_light,
-    "positronDataExplorer.foreground": p.gray_light,
-    "positronDataExplorer.background": p.bg,
+    "positronConsole.background": p.bg_1,
+    "positronVariables.foreground": p.fg_1,
+    "positronVariables.background": p.bg_1,
+    "positronVariables.activeSelectionBackground": p.purple_2,
+    "positronActionBar.selectBoxBackground": p.bg_2,
+    "positronActionBar.selectBorder": p.bg_3,
+    "positronDataExplorer.foreground": p.fg_3,
+    "positronDataExplorer.background": p.bg_2,
 
-    "activityBar.background": p.bg_deep,
-    "activityBar.activeBackground": p.purple,
+    "activityBar.background": p.bg_1,
+    "activityBar.activeBackground": p.purple_2,
     "activityBar.foreground": p.white,
     "activityBar.inactiveForeground": p.white + "99",
-    "activityBarBadge.background": p.purple,
+    "activityBarBadge.background": p.purple_2,
     "activityBarBadge.foreground": p.white,
 
-    "sideBar.background": p.bg_deep,
-    "sideBar.foreground": p.gray,
-    "sideBarTitle.background": p.bg,
-    "sideBarSectionHeader.background": p.bg_light,
-    "sideBarSectionHeader.foreground": p.gray_light,
+    "sideBar.background": p.bg_1,
+    "sideBar.foreground": p.fg_1,
+    "sideBarTitle.background": p.bg_2,
+    "sideBarSectionHeader.background": p.bg_3,
+    "sideBarSectionHeader.foreground": p.fg_3,
 
-    "titleBar.activeBackground": p.purple,
+    "titleBar.activeBackground": p.purple_2,
     "titleBar.activeForeground": p.white,
-    "titleBar.inactiveBackground": p.bg_deep,
+    "titleBar.inactiveBackground": p.bg_1,
     "titleBar.inactiveForeground": p.white + "99",
 
-    "statusBar.background": p.purple,
+    "statusBar.background": p.purple_2,
     "statusBar.foreground": p.white,
-    "statusBar.debuggingBackground": p.purple_bright,
+    "statusBar.debuggingBackground": p.purple_3,
 
-    "dropdown.background": p.bg,
-    "dropdown.foreground": p.gray_light,
-    "dropdown.border": p.bg_light,
+    "dropdown.background": p.bg_2,
+    "dropdown.foreground": p.fg_3,
+    "dropdown.border": p.bg_3,
 
-    "input.background": p.bg_deep,
-    "input.foreground": p.gray_light,
+    "input.background": p.bg_1,
+    "input.foreground": p.fg_3,
     "input.placeholderForeground": p.white + "60",
-    "input.border": p.bg_light,
-    "inputOption.activeBorder": p.purple,
-    "inputOption.activeBackground": p.purple + "80",
-    "inputValidation.infoBorder": p.purple,
+    "input.border": p.bg_3,
+    "inputOption.activeBorder": p.purple_2,
+    "inputOption.activeBackground": p.purple_2 +"80",
+    "inputValidation.infoBorder": p.purple_2,
 
-    "list.activeSelectionBackground": p.purple,
-    "list.hoverBackground": p.bg_light,
-    "list.focusBackground": p.bg_light,
-    "list.inactiveSelectionBackground": p.bg_light,
-    "list.highlightForeground": p.purple_light,
-    "list.focusOutline": p.purple,
+    "list.activeSelectionBackground": p.purple_2,
+    "list.hoverBackground": p.bg_3,
+    "list.focusBackground": p.bg_3,
+    "list.inactiveSelectionBackground": p.bg_3,
+    "list.highlightForeground": p.purple_4,
+    "list.focusOutline": p.purple_2,
 
-    "focusBorder": p.purple,
+    "focusBorder": p.purple_2,
 
-    "button.background": p.purple,
+    "button.background": p.purple_2,
     "button.foreground": p.white,
-    "button.hoverBackground": p.purple_bright,
+    "button.hoverBackground": p.purple_3,
 
-    "progressBar.background": p.purple,
+    "progressBar.background": p.purple_2,
 
-    "textLink.foreground": p.purple_light,
-    "textLink.activeForeground": p.purple_light,
+    "textLink.foreground": p.purple_4,
+    "textLink.activeForeground": p.purple_4,
 
-    "pickerGroup.foreground": p.purple_light,
+    "pickerGroup.foreground": p.purple_4,
 
-    "settings.focusedRowBorder": p.purple,
-    "settings.modifiedItemIndicator": p.purple_bright,
+    "settings.focusedRowBorder": p.purple_2,
+    "settings.modifiedItemIndicator": p.purple_3,
 
-    "notifications.border": p.bg_light,
-    "notifications.background": p.bg,
-    "notifications.foreground": p.gray_light,
-    "notificationCenterHeader.background": p.bg_light,
+    "notifications.border": p.bg_3,
+    "notifications.background": p.bg_2,
+    "notifications.foreground": p.fg_3,
+    "notificationCenterHeader.background": p.bg_3,
 
-    "sash.hoverBorder": p.purple,
+    "sash.hoverBorder": p.purple_2,
 
-    "peekView.border": p.purple,
-    "peekViewTitle.background": p.bg_light,
-    "peekViewResult.background": p.bg_deep,
-    "peekViewEditor.background": p.bg,
+    "peekView.border": p.purple_2,
+    "peekViewTitle.background": p.bg_3,
+    "peekViewResult.background": p.bg_1,
+    "peekViewEditor.background": p.bg_2,
 
-    "badge.background": p.purple,
+    "badge.background": p.purple_2,
     "badge.foreground": p.white,
 
-    "menu.background": p.bg,
-    "menu.foreground": p.gray_light,
-    "menu.selectionBackground": p.purple,
+    "menu.background": p.bg_2,
+    "menu.foreground": p.fg_3,
+    "menu.selectionBackground": p.purple_2,
     "menu.selectionForeground": p.white,
-    "menu.separatorBackground": p.bg_light,
+    "menu.separatorBackground": p.bg_3,
 
-    "commandCenter.background": p.bg_deep,
-    "commandCenter.foreground": p.gray_light,
-    "commandCenter.border": p.bg_light + "80",
+    "commandCenter.background": p.bg_1,
+    "commandCenter.foreground": p.fg_3,
+    "commandCenter.border": p.bg_3 + "80",
 
-    "checkbox.background": p.bg_deep,
-    "checkbox.border": p.purple_muted,
+    "checkbox.background": p.bg_1,
+    "checkbox.border": p.purple_1,
 
-    "scrollbarSlider.background": p.purple_muted + "66",
-    "scrollbarSlider.hoverBackground": p.purple + "b3",
-    "scrollbarSlider.activeBackground": p.purple_bright + "b3",
+    "scrollbarSlider.background": p.purple_1 + "66",
+    "scrollbarSlider.hoverBackground": p.purple_2 +"b3",
+    "scrollbarSlider.activeBackground": p.purple_3 + "b3",
 
     "breadcrumb.foreground": p.white + "b3",
-    "breadcrumb.focusForeground": p.gray_light,
+    "breadcrumb.focusForeground": p.fg_3,
     "breadcrumb.activeSelectionForeground": p.white,
 
     "descriptionForeground": p.white + "b3",
-    "icon.foreground": p.gray,
+    "icon.foreground": p.fg_1,
 
-    "keybindingLabel.background": p.bg_light + "2b",
-    "keybindingLabel.foreground": p.gray_light,
-    "keybindingLabel.border": p.purple_muted + "99",
+    "keybindingLabel.background": p.bg_3 + "2b",
+    "keybindingLabel.foreground": p.fg_3,
+    "keybindingLabel.border": p.purple_1 + "99",
+
+    "editorRuler.foreground": p.purple_1,
   },
 
   "tokenColors": [
     {
-      "name": "Text and Source Base",
-      "scope": ["text", "source"],
-      "settings": { "foreground": p.text }
-    },
-    {
-      "name": "Variable",
-      "scope": ["variable", "variable.parameter", "source.python", "source.python meta.function-call.arguments.python"],
-      "settings": { "foreground": p.white }
-    },
-    {
-      "name": "Constant Numeric",
-      "scope": ["constant.numeric"],
-      "settings": { "foreground": p.red_light }
-    },
-    {
-      "name": "Constant Language",
-      "scope": ["constant.language"],
-      "settings": { "foreground": p.orange, "fontStyle": "bold" }
-    },
-    {
-      "name": "Constant Other",
-      "scope": ["constant"],
-      "settings": { "foreground": p.salmon }
-    },
-    {
-      "name": "String",
-      "scope": ["string", "constant.other.symbol", "entity.other.inherited-class"],
-      "settings": { "foreground": p.green }
-    },
-    {
-      "name": "Punctuation Separator / Operator",
-      "scope": ["punctuation.separator.arguments.python", "punctuation.separator.element.python", "punctuation.separator.period.python", "keyword.operator"],
-      "settings": { "foreground": p.cyan, "fontStyle": "" }
-    },
-    {
-      "name": "Keyword",
-      "scope": ["keyword"],
-      "settings": { "foreground": p.purple_light, "fontStyle": "bold" }
-    },
-    {
-      "name": "Storage",
-      "scope": ["storage"],
-      "settings": { "foreground": p.purple_light }
-    },
-    {
-      "name": "Function",
-      "scope": ["entity.name.function", "meta.function-call support.type", "meta.function-call support.function", "meta.function-call.generic", "support.function", "keyword.other.special-method", "variable.function"],
-      "settings": { "foreground": p.blue }
-    },
-    {
       "name": "Comment",
       "scope": ["comment", "punctuation.definition.comment"],
-      "settings": { "foreground": p.purple_bright }
+      "settings": { "foreground": p.purple_3 }
     },
-    {
-      "name": "Punctuation Definition",
-      "scope": ["punctuation.definition.string", "punctuation.definition.variable", "punctuation.definition.parameters", "punctuation.definition.array"],
-      "settings": { "foreground": p.green_pale }
-    },
-    {
-      "name": "Class",
-      "scope": ["support.class", "entity.name.class", "entity.name.type.class", "variable.language.this.js"],
-      "settings": { "foreground": p.gold }
-    },
-    {
-      "name": "Meta Class",
-      "scope": ["meta.class"],
-      "settings": { "foreground": p.white }
-    },
-    {
-      "name": "Support (tags, misc)",
-      "scope": ["constant.other.color", "meta.tag", "punctuation.definition.tag", "punctuation.separator.inheritance.php", "punctuation.definition.tag.html", "punctuation.definition.tag.begin.html", "punctuation.definition.tag.end.html"],
-      "settings": { "foreground": p.teal }
-    },
-    {
-      "name": "Tag Name",
-      "scope": ["entity.name.tag"],
-      "settings": { "foreground": p.red_pink }
-    },
-    {
-      "name": "Attribute",
-      "scope": ["entity.other.attribute-name"],
-      "settings": { "foreground": p.gold }
-    },
-    {
-      "name": "Attribute ID",
-      "scope": ["entity.other.attribute-name.id"],
-      "settings": { "foreground": p.yellow_bright }
-    },
-    {
-      "name": "Heading",
-      "scope": ["markup.heading punctuation.definition.heading", "entity.name.section"],
-      "settings": { "foreground": p.blue_muted, "fontStyle": "" }
-    },
-    {
-      "name": "Bold",
-      "scope": ["markup.bold", "punctuation.definition.bold"],
-      "settings": { "foreground": p.gold, "fontStyle": "bold" }
-    },
-    {
-      "name": "Italic",
-      "scope": ["markup.italic", "punctuation.definition.italic"],
-      "settings": { "foreground": p.lavender, "fontStyle": "italic" }
-    },
-    {
-      "name": "Markup Fenced Code / Quarto Raw",
-      "scope": ["markup.fenced_code.block.markdown", "markup.inline.raw.string.quarto"],
-      "settings": { "foreground": p.green }
-    },
-    {
-      "name": "Quarto Bold/Italic (no style override)",
-      "scope": ["markup.bold.quarto", "markup.italic.quarto"],
-      "settings": { "fontStyle": "" }
-    },
-    {
-      "name": "Quarto Shortcode Import",
-      "scope": ["keyword.control.import.shortcode"],
-      "settings": { "foreground": p.white }
-    },
-    {
-      "name": "Quarto Div Heading",
-      "scope": ["markup.heading.div.quarto"],
-      "settings": { "foreground": p.purple_light, "fontStyle": "bold" }
-    },
-    {
-      "name": "Markup List",
-      "scope": ["markup.list"],
-      "settings": { "foreground": p.text }
-    },
-    {
-      "name": "SQL Database/Table Names",
-      "scope": ["constant.other.database-name.sql", "constant.other.table-name.sql"],
-      "settings": { "foreground": p.blue }
-    },
-    {
-      "name": "Inline Code",
-      "scope": ["markup.raw.inline"],
-      "settings": { "foreground": p.yellow_green }
-    },
-    {
-      "name": "Link Text",
-      "scope": ["string.other.link", "punctuation.definition.string.end.markdown"],
-      "settings": { "foreground": p.red_pink }
-    },
-    {
-      "name": "Link URL",
-      "scope": ["meta.link"],
-      "settings": { "foreground": p.salmon }
-    },
-    {
-      "name": "Markup Quote",
-      "scope": ["markup.quote"],
-      "settings": { "foreground": p.salmon }
-    },
-    {
-      "name": "Separator",
-      "scope": ["meta.separator"],
-      "settings": { "foreground": p.green_pale }
-    },
-    {
-      "name": "Markup Inserted",
-      "scope": ["markup.inserted"],
-      "settings": { "foreground": p.yellow_green }
-    },
-    {
-      "name": "Markup Deleted",
-      "scope": ["markup.deleted"],
-      "settings": { "foreground": p.red_pink }
-    },
-    {
-      "name": "Markup Changed",
-      "scope": ["markup.changed"],
-      "settings": { "foreground": p.lavender }
-    },
-    {
-      "name": "Regular Expression",
-      "scope": ["string.regexp"],
-      "settings": { "foreground": p.teal }
-    },
-    {
-      "name": "Escape Character",
-      "scope": ["constant.character.escape"],
-      "settings": { "foreground": p.salmon }
-    },
-    {
-      "name": "Embedded",
-      "scope": ["punctuation.section.embedded", "variable.interpolation"],
-      "settings": { "foreground": p.red_dark }
-    },
-    {
-      "name": "Invalid Illegal",
-      "scope": ["invalid.illegal"],
-      "settings": { "foreground": p.white }
-    },
-    {
-      "name": "Invalid Broken",
-      "scope": ["invalid.broken"],
-      "settings": { "foreground": p.near_black }
-    },
-    {
-      "name": "Invalid Deprecated",
-      "scope": ["invalid.deprecated"],
-      "settings": { "foreground": p.white }
-    },
-    {
-      "name": "CSS Property Name",
-      "scope": ["support.type.property-name", "meta.property-list meta.property-name"],
-      "settings": { "foreground": p.teal }
-    },
-    {
-      "name": "CSS Units",
-      "scope": ["keyword.other.unit", "source.css keyword.other.unit", "source.less keyword.other.unit", "source.scss keyword.other.unit"],
-      "settings": { "foreground": p.yellow_pale }
-    },
-    {
-      "name": "CSS Color Hash",
-      "scope": ["constant.other.color.rgb-value punctuation.definition.constant"],
-      "settings": { "foreground": p.salmon }
-    },
-    {
-      "name": "CSS Colors Named",
-      "scope": ["constant.other.color", "meta.property-value support.constant.named-color.css"],
-      "settings": { "foreground": p.yellow_pale }
-    },
-    {
-      "name": "Selector",
-      "scope": ["meta.selector"],
-      "settings": { "foreground": p.lavender }
-    },
-    {
-      "name": "Git Gutter Deleted",
-      "scope": ["markup.deleted.git_gutter"],
-      "settings": { "foreground": p.red_coral }
-    },
-    {
-      "name": "Git Gutter Changed",
-      "scope": ["markup.changed.git_gutter"],
-      "settings": { "foreground": p.yellow_git }
-    },
-    {
-      "name": "Git Gutter Inserted",
-      "scope": ["markup.inserted.git_gutter"],
-      "settings": { "foreground": p.green_soft }
-    },
-    {
-      "name": "Find in Files Path",
-      "scope": ["text.find-in-files entity.name.filename.find-in-files"],
-      "settings": { "foreground": p.yellow_pale }
-    }
-  ]
+  ],
 };
 
 fs.writeFileSync(
